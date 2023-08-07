77205da5067ae2b40eec65d2ec2a645a26c8a816
Andriy Konoval <ceperiv@gmail.com>
2023-08-07 19:09:04 +0300

    nginx.conf

--- src/environmets/environment.prod.ts
+++ src/environmets/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key:'live_yOA1f9dB76pd8QCGKELWIKcG7MEOepaQzziB4BjyypjuQ6SjbNkZ4FCwY7rl3dgm',
+  API:'https://api.thecatapi.com/v1/images',
+  breeds:'https://api.thecatapi.com/v1/breeds',
 }
